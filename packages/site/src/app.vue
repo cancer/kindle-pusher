623c88793e1e25161aaf5daad5894ba30de5de45
cancer <zodiac.cancer.j6@gmail.com>
2019-10-27 16:14:15 +0900

    parcel + vue + ts

---
+++ packages/site/src/app.vue
@@ -0,0 +1,9 @@
+<template functional>
+  <p>Hello World</p>
+</template>
+
+<script lang="ts">
+  console.log('Hello World')
+  export default {
+  }
+</script>
---
+++ packages/site/src/index.ts
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import app from './app';
+
+new Vue({
+  el: '#root',
+  template: '<app />',
+  components: { app },
+  data: {},
+})
