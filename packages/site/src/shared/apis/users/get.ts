9a42d9e9d9b7dfb24d4e19574cd8627619ef2abb
cancer <zodiac.cancer.j6@gmail.com>
2019-10-28 20:19:41 +0900

    redirect functions to api

--- packages/site/src/shared/apis/users/get.ts
+++ packages/site/src/shared/apis/users/get.ts
@@ -1,11 +1,12 @@
 import { getToken } from "../../../domains/auth/get-token";
+import { basePath } from "../base-path";
 import { UserResponse } from "./response";
 
 export const usersGetApi = async (): Promise<UserResponse> => {
   const token = await getToken();
   
   try {
-    const res = await fetch(`/.netlify/functions/users?authorization=${token}`);
+    const res = await fetch(`${basePath}/users?authorization=${token}`);
     return await res.json();
   } catch(e) {
     throw e;
