19a185de00deec16b0b75a3d087b23899a05f887
cancer <zodiac.cancer.j6@gmail.com>
2019-11-02 17:01:13 +0900

    debug

--- packages/functions/src/handlers/users/get.ts
+++ packages/functions/src/handlers/users/get.ts
@@ -15,50 +15,52 @@ export interface UserDocument {
 
 export const handleUsersGet = async (event: APIGatewayEvent) => {
   let authToken: AuthToken;
-  //try {
-  //  if (event.headers['cookie'] === null) {
-  //    throw new Error('Authorization required.')
-  //  }
-  //  const token = parse(event.headers['cookie']).token;
-  //  authToken = await makeAuthToken(token);
-  //} catch(e) {
-  //  return makeErrorResponse(401, e);
-  //}
-  //
-  //if (!authToken.isVerified) {
-  //  return makeErrorResponse(401, new Error('Authorization required.'));
-  //}
-  //
-  //const client = container.get(FaunadbProvider).provide();
-  //console.log(await client.ping('read'))
-  //const userRef = query.Match(query.Index('users_by_id'), authToken.id);
-  //
-  //try {
-  //  const exists = await client.query<boolean>(query.Exists(userRef));
-  //  if (!exists) {
-  //    return {
-  //      statusCode: 200,
-  //      body: JSON.stringify({
-  //        user: null,
-  //      }),
-  //    };
-  //  }
-  //
-  //  const user = await client.query<values.Document<UserDocument>>(query.Get(userRef));
-  //  return {
-  //    statusCode: 200,
-  //    body: JSON.stringify({
-  //      user: user.data,
-  //    }),
-  //  }
-  //} catch(e) {
-  //  console.log(e)
-  //  return makeErrorResponse(500, e);
-  //}
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      result: 'debug',
-    })
-  };
+  try {
+    if (event.headers['cookie'] === null) {
+      throw new Error('Authorization required.')
+    }
+    const token = parse(event.headers['cookie']).token;
+    authToken = await makeAuthToken(token);
+  } catch(e) {
+    return makeErrorResponse(401, e);
+  }
+  
+  if (!authToken.isVerified) {
+    return makeErrorResponse(401, new Error('Authorization required.'));
+  }
+  
+  const client = container.get(FaunadbProvider).provide();
+  const userRef = query.Match(query.Index('users_by_id'), authToken.id);
+  
+  try {
+    const exists = await client.query<boolean>(query.Exists(userRef));
+    if (!exists) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          user: null,
+        }),
+      };
+    }
+  
+    const user = await client.query<values.Document<UserDocument>>(query.Get(userRef));
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        user: user.data,
+      }),
+    }
+  } catch(e) {
+    console.log(e)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: {
+          ...e,
+          ping: await client.ping('read'),
+        }
+      })
+    }
+    //return makeErrorResponse(500, e);
+  }
 }
