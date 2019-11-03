901a7dd03ff9ce33824959c5f30c52f6d7c6872a
cancer <zodiac.cancer.j6@gmail.com>
2019-11-03 19:48:18 +0900

    remove handleBooksGet

--- packages/functions/src/handlers/books/index.ts
+++ packages/functions/src/handlers/books/index.ts
@@ -4,6 +4,7 @@ import { query, values } from "faunadb";
 import { injectable } from "inversify";
 import { makeAuthToken } from "../../domains/auth-token";
 import { AuthToken } from "../../domains/auth-token/auth-token";
+import { AuthorizationError } from "../../domains/error/authorization";
 import { HttpMethodNotAllowedError } from "../../domains/error/method-not-allowed";
 import { getStatusCodeByError } from "../../lib/response/get-status-code-by-error";
 import { FaunadbProvider } from "../../shared/faunadb-provider";
@@ -24,48 +25,6 @@ interface BookshelfDocument {
   title: string;
   detailPageUrl: string;
   productUrl: string;
-}
-
-const handleBooksGet = async (event: APIGatewayEvent) => {
-  let authToken: AuthToken;
-  try {
-    if (event.headers['cookie'] === null) {
-      throw new Error('Authorization required.')
-    }
-    const token = parse(event.headers['cookie']).token;
-    authToken = await makeAuthToken(token);
-  } catch(e) {
-    return makeErrorResponse(401, e);
-  }
-  
-  const client = container.get(FaunadbProvider).provide();
-  const bookRefs = await client.query<Lambda<values.Document<BookDocument>>>(
-    query.Map(
-      query.Paginate(
-        query.Match(
-          query.Index('books_by_user'),
-          authToken.id,
-        )
-      ),
-      query.Lambda('X', query.Get(query.Var('X'))),
-    ),
-  );
-  
-  const books = await Promise.all(bookRefs.data.map(bookRef => {
-    return client.query<values.Document<BookshelfDocument>>(
-      query.Get(
-        query.Match(
-          query.Index('bookshelf_by_asin'),
-          bookRef.data.asin,
-        ),
-      ),
-    );
-  }));
-  
-  return {
-    statusCode: 200,
-    body: JSON.stringify(books.map(v => v.data)),
-  };
 }
 
 @injectable()
@@ -91,6 +50,38 @@ export class BooksHandler {
   }
   
   private async handleGet(event: APIGatewayEvent): Promise<APIGatewayProxyResult> {
-    return handleBooksGet(event);
+    if (event.queryStringParameters === null) {
+      throw new AuthorizationError();
+    }
+  
+    const authToken = await makeAuthToken(event.queryStringParameters.authorization);
+    const client = container.get(FaunadbProvider).provide();
+    const bookRefs = await client.query<Lambda<values.Document<BookDocument>>>(
+      query.Map(
+        query.Paginate(
+          query.Match(
+            query.Index('books_by_user'),
+            authToken.id,
+          )
+        ),
+        query.Lambda('X', query.Get(query.Var('X'))),
+      ),
+    );
+  
+    const books = await Promise.all(bookRefs.data.map(bookRef => {
+      return client.query<values.Document<BookshelfDocument>>(
+        query.Get(
+          query.Match(
+            query.Index('bookshelf_by_asin'),
+            bookRef.data.asin,
+          ),
+        ),
+      );
+    }));
+  
+    return {
+      statusCode: 200,
+      body: JSON.stringify(books.map(v => v.data)),
+    };
   }
 }
