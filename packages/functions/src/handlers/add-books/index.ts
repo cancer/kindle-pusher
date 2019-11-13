d432b2c0a6624b18f95e41b4cc31fea342d99009
cancer <zodiac.cancer.j6@gmail.com>
2019-11-13 13:32:54 +0900

    add AddBooksCommands

--- packages/functions/src/handlers/add-books/index.ts
+++ packages/functions/src/handlers/add-books/index.ts
@@ -1,151 +1,14 @@
 import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
-import { query, values } from "faunadb";
 import { injectable } from "inversify";
-import { DateTime } from "luxon";
-import fetch from 'node-fetch';
 import { HttpMethodNotAllowedError } from "../../domains/error/method-not-allowed";
-import { LambdaResult } from "../../lib/db/lambda-result";
-import { UserDocument } from "../../lib/db/user";
 import { getStatusCodeByError } from "../../lib/response/get-status-code-by-error";
 import { makeErrorResponse } from "../../lib/response/make-error-response";
-import { FaunadbProvider } from "../../shared/faunadb-provider";
-import { container } from "../../shared/inversify.config";
-
-interface Book {
-  acquisitionDate: number;
-  asin: string;
-  authors: string[];
-  detailPageUrl: string;
-  lastAnnotationDate: number;
-  percentageRead: number;
-  productUrl: string;
-  title: string;
-  webReaderUrl: string;
-}
-
-interface Books {
-  length: number;
-  itemsList: Book[];
-}
-
-interface BookData {
-  asin: string;
-  title: string;
-  detailPageUrl: string;
-  productUrl: string;
-  user: string;
-}
-
-const getNewArrivals = async () => {
-  const res = await fetch('https://kindle-newer-book.netlify.com/books.json');
-  const json = await res.json();
-  return json.itemsList;
-}
-
-const getBooks = async (users: UserDocument[]): Promise<BookData[]> => {
-  const result = await Promise.all(users.map(async user => {
-    const res = await fetch(user.bookShelfApi);
-    const json = await res.json() as Books;
-    return json.itemsList.map(book => {
-      return {
-        asin: book.asin,
-        title: book.title,
-        detailPageUrl: book.detailPageUrl,
-        productUrl: book.productUrl,
-        user: user.auth0Id,
-      };
-    });
-  }));
-  
-  return result.flat();
-};
-
-const createBookshelf = async (book: BookData) => {
-  const client = container.get(FaunadbProvider).provide();
-  const bookRef = query.Match(query.Index('bookshelf_by_asin'), book.asin);
-  const dateTime = DateTime.local().toISO();
-  
-  console.log(`${book.asin} ${(await client.query<boolean>(query.Exists(bookRef))) ? 'has been registered.' : 'is not registered.'}`);
-  if (!await client.query<boolean>(query.Exists(bookRef))) {
-    console.log(`--- Create bookshelf: ${book.asin}`);
-    client.query(query.Create(query.Collection('bookshelf'), {
-      data: {
-        asin: book.asin,
-        title: book.title,
-        detailPageUrl: book.detailPageUrl,
-        productUrl: book.productUrl,
-        createdAt: dateTime,
-        updatedAt: dateTime,
-      }
-    }));
-  }
-};
-
-const createBooks = async (book: BookData) => {
-  const client = container.get(FaunadbProvider).provide();
-  const bookRef = query.Match(query.Index('all_books'), [book.asin, book.user]);
-  const dateTime = DateTime.local().toISO();
-  
-  if (!await client.query<boolean>(query.Exists(bookRef))) {
-    console.log(`--- Create book: ${book.asin} by ${book.user}`);
-    client.query(query.Create(query.Collection('books'), {
-      data: {
-        user: book.user,
-        asin: book.asin,
-        ignored: false,
-        createdAt: dateTime,
-        updatedAt: dateTime,
-      },
-    }));
-  }
-};
-
-const getUserBookData = async (): Promise<BookData[]> => {
-  const client = container.get(FaunadbProvider).provide();
-  console.log('--- Get user bookshelf apis')
-  const users = await client.query<LambdaResult<values.Document<UserDocument>>>(
-    query.Map(
-      query.Paginate(
-        query.Match(
-          query.Index('all_users'),
-        )
-      ),
-      query.Lambda('X', query.Get(query.Var('X'))),
-    )
-  );
-  
-  return getBooks(
-    users.data.map(user => {
-      return user.data;
-    }).filter(v => !!v.bookShelfApi)
-  );
-}
-
-export const handleAddBooksPost = async () => {
-  try {
-    const books = await getUserBookData();
-    books.forEach(async book => {
-      await createBookshelf(book);
-      await createBooks(book);
-    });
-    
-    return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
-      },
-      body: JSON.stringify({
-        result: books,
-      })
-    }
-  } catch(e) {
-    console.error(e);
-    return makeErrorResponse(500, e);
-  }
-}
+import { AddBooksCommands } from "./commands";
 
 @injectable()
 export class AddBooksHandler {
+  constructor(private readonly commands: AddBooksCommands) {}
+  
   async handle(ev: APIGatewayEvent): Promise<APIGatewayProxyResult> {
     try {
       if (ev.httpMethod === 'POST') {
@@ -163,7 +26,19 @@ export class AddBooksHandler {
     }
   }
   
-  private handlePost(): Promise<APIGatewayProxyResult> {
-    return handleAddBooksPost()
+  private async handlePost(): Promise<APIGatewayProxyResult> {
+    try {
+      await this.commands.addBooks();
+      
+      return {
+        statusCode: 204,
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+        },
+        body: JSON.stringify({})
+      }
+    } catch(e) {
+      throw e;
+    }
   }
 }
